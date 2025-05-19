cb800aadd83b51c4fea30fdeda0c72080a4906fa
Thiago Diegoli <thiago_diegoli@hotmail.com>
2025-05-19 17:34:08 -0300

    fix cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from auth import register, login
 from encrypt import encrypt_message, decrypt_message
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/register', methods=['POST'])
 def register_user():
